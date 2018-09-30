a44cb73acf9fea940840b2d403b49191d086fb42
salamri <42224359+salamri@users.noreply.github.com>
2018-09-30 04:32:50 +0300

    Update bot5.js

--- bot5.js
+++ bot5.js
@@ -69,4 +69,29 @@ client.on("message", async message => {
 
 
 
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('+bc')) {
+          if (!args[0]) {
+message.channel.send("**+bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
+
+
+
  client.login(process.env.BOT_TOKEN5);
